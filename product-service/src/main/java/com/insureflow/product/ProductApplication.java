2143bae8eb28c9f8528fbb64e7be3b09b618cc45
erlanggariansyah <erlanggariansyah0@gmail.com>
2026-03-29 11:56:03 +0700

    Initial Commit

--- product-service/src/main/java/com/insureflow/product/ProductApplication.java
+++ product-service/src/main/java/com/insureflow/product/ProductApplication.java
@@ -3,7 +3,9 @@ package com.insureflow.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @EnableCaching
 @SpringBootApplication
 public class ProductApplication {
