35626b90f33a0b4fe94a4a0a1f8e2ed7507c0a9a
erlanggariansyah <erlanggariansyah0@gmail.com>
2026-03-30 04:58:54 +0700

    Initial Commit

--- product-service/src/main/java/com/insureflow/product/constant/AttributeConstant.java
+++ product-service/src/main/java/com/insureflow/product/constant/AttributeConstant.java
@@ -8,4 +8,9 @@ public class AttributeConstant {
     public static final String RIDER_ID = "riderId";
     public static final String AGE = "age";
     public static final String GENDER = "gender";
+
+    public static final String MESSAGE = "message";
+    public static final String STATUS = "status";
+    public static final String TRACE_ID = "traceId";
+    public static final String DATA = "data";
 }
--- product-service/src/main/java/com/insureflow/product/util/APIResponse.java
+++ product-service/src/main/java/com/insureflow/product/util/APIResponse.java
@@ -1,13 +1,16 @@
 package com.insureflow.product.util;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.insureflow.product.constant.AttributeConstant;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonPropertyOrder({AttributeConstant.STATUS, AttributeConstant.MESSAGE, AttributeConstant.TRACE_ID, AttributeConstant.DATA})
 public class APIResponse<T> {
-    private String message;
     private int status;
+    private String message;
     private String traceId;
     private T data;
 }
