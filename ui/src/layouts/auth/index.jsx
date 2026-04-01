7e6800d65ac27f54c9ee6d3fae3515ec5991a620
erlanggariansyah <erlanggariansyah0@gmail.com>
2026-04-02 06:08:42 +0700

    Initial Commit

--- ui/src/layouts/auth/index.jsx
+++ ui/src/layouts/auth/index.jsx
@@ -1,6 +1,5 @@
-import Footer from "components/footer/FooterAuthDefault";
-import authImg from "assets/img/auth/auth.png";
-import { Link, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import logo from "../../assets/img/logo.png";
 import routes from "routes.js";
 import FixedPlugin from "components/fixedPlugin/FixedPlugin";
 
@@ -16,34 +15,17 @@ export default function Auth() {
       }
     });
   };
+  
   document.documentElement.dir = "ltr";
+  
   return (
     <div>
-      <div className="relative float-right h-full min-h-screen w-full !bg-white dark:!bg-navy-900">
-        <FixedPlugin />
+      <div className="relative float-right h-full min-h-screen w-full bg-white dark:bg-navy-900">
         <main className={`mx-auto min-h-screen`}>
           <div className="relative flex">
-            <div className="mx-auto flex min-h-full w-full flex-col justify-start pt-12 md:max-w-[75%]  lg:max-w-[1013px] lg:px-8 lg:pt-0 xl:min-h-[100vh] xl:max-w-[1383px] xl:px-0 xl:pl-[70px]">
-              <div className="mb-auto flex flex-col pl-5 pr-5 md:pr-0 md:pl-12 lg:max-w-[48%] lg:pl-0 xl:max-w-full">
-                <Link to="/admin" className="mt-0 w-max lg:pt-10">
-                  <div className="mx-auto flex h-fit w-fit items-center hover:cursor-pointer">
-                    <svg
-                      width="8"
-                      height="12"
-                      viewBox="0 0 8 12"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M6.70994 2.11997L2.82994 5.99997L6.70994 9.87997C7.09994 10.27 7.09994 10.9 6.70994 11.29C6.31994 11.68 5.68994 11.68 5.29994 11.29L0.709941 6.69997C0.319941 6.30997 0.319941 5.67997 0.709941 5.28997L5.29994 0.699971C5.68994 0.309971 6.31994 0.309971 6.70994 0.699971C7.08994 1.08997 7.09994 1.72997 6.70994 2.11997V2.11997Z"
-                        fill="#A3AED0"
-                      />
-                    </svg>
-                    <p className="ml-3 text-sm text-gray-600">
-                      Back to Dashboard
-                    </p>
-                  </div>
-                </Link>
+            {/* Sisi Kiri: Area Login Form */}
+            <div className="mx-auto flex min-h-full w-full flex-col justify-start md:max-w-[75%] lg:max-w-[1013px] lg:px-8 xl:min-h-[100vh] xl:max-w-[1383px] xl:px-0">
+              <div className="flex flex-col h-screen justify-center pl-5 pr-5 md:pr-0 md:pl-0 lg:max-w-[48%] xl:max-w-full">
                 <Routes>
                   {getRoutes(routes)}
                   <Route
@@ -51,14 +33,22 @@ export default function Auth() {
                     element={<Navigate to="/auth/sign-in" replace />}
                   />
                 </Routes>
-                <div className="absolute right-0 hidden h-full min-h-screen md:block lg:w-[49vw] 2xl:w-[44vw]">
-                  <div
-                    className="absolute flex h-full w-full items-end justify-center bg-cover bg-center lg:rounded-bl-[120px] xl:rounded-bl-[200px]"
-                    style={{ backgroundImage: `url(${authImg})` }}
+              </div>
+            </div>
+
+            {/* Sisi Kanan: Style OpenLife & Logo */}
+            <div className="absolute right-0 hidden h-full min-h-screen md:block lg:w-[49vw] 2xl:w-[44vw]">
+              <div
+                className="absolute flex h-full w-full items-center justify-center bg-[#344e86]"
+              >
+                <div className="flex flex-col items-center">
+                  <img 
+                    src={logo} 
+                    alt="OpenLife Logo" 
+                    className="w-96 h-96 object-contain mb-4 filter brightness-0 invert" 
                   />
                 </div>
               </div>
-              <Footer />
             </div>
           </div>
         </main>
